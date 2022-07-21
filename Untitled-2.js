9f8e5e1af0c1ca30a7c20ee501032fe8400bb68e
Nikita.Solo <solodovnik.mykyta@icloud.com>
2022-07-21 17:03:59 +0200

    hm

---
+++ Untitled-2.js
@@ -0,0 +1,88 @@
+//var worker = new Worker ('Tonny', 'Stark', 10, 31);
+//console.log(.worker.name);
+//console.log(worker.surname);
+//console.log(worker.rate);
+//console.log(worker.days);
+//console.log(worker.getSalary());
+
+//constructor(worker.name,worcer.surname, rate, days) {
+//this.name = worker.name ;
+//this.surname = surname;
+//this.rate = rate;
+//this.days = days;
+
+//}
+//getName()
+
+//getSurname() {
+//    return this.surname;
+//}
+//getRate() {
+  //  return this.rate;
+//}
+//getDays() {
+   // return this.days;
+//}
+//getSalary(rate, days) {
+   // let salary = this.rate * this.days;
+   // return salary;
+//}
+
+
+//constructor(worker.name, surname, rate, days) {
+   // this.name = worker.name;
+   // this.surname = surname;
+  //  this.rate = rate;
+  //  this.days = days;
+//}
+//getName() {
+ //   return this.name;
+//}
+//getSurname() {
+ //   return this.surname;
+//}
+//getRate() {
+ //   return this.rate;
+//}
+//setRate(rate) {
+ //   this.rate = rate;
+//}
+//getDays() {
+ //   return this.days;
+//}
+//setDays(days) {
+  //  this.days = days;
+//}
+//getSalary(rate, days) {
+  //  let salary = this.rate * this.#days;
+   // return salary;
+//}
+
+//let workerThree = new WorkerTypeThree('Tonny', 'Stark', 10, 31);
+//console.log(workerThree.getRate());
+//console.log(workerThree.getDays());
+//console.log(workerThree.getSalary());
+//workerThree.setRate(40);
+//workerThree.setDays(40);
+//console.log(workerThree.getRate());
+//console.log(workerThree.getDays());
+//console.log(workerThree.getSalary());
+
+//var str = new MyString();
+//console.log(str.reverse('abcade'));
+//console.log(str.ucFirst('abcade'))
+//console.log(str.Words('abcade abcade abcade'))
+
+var student = new Student('Tonny','Stark', 2016);
+console.log(worker.name);
+console.log(worker.surname);
+console.log(worker.getFullName());
+console.log(worker.year);
+class User{
+   construktor(name,surname){
+    this.surname = surname;
+   }
+
+   getFullName(){
+     return this.name + ' ' + this.surname;
+   }
